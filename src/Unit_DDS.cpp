f0a6c9ae3f89d94d42b466d6c5560ae65f4cfeac
GOB <goblin52@gmail.com>
2025-08-28 15:26:35 +0900

    Add and fixes files for M5UnitUnified

--- src/Unit_DDS.cpp
+++ src/Unit_DDS.cpp
@@ -1,7 +1,9 @@
 #include "Unit_DDS.h"
+#include <cstdio>
 
 /*! @brief Write data to the DDS.*/
-void Unit_DDS::writeDDSReg(uint8_t addr, uint8_t data) {
+void Unit_DDS::writeDDSReg(uint8_t addr, uint8_t data)
+{
     // Serial.printf("ADDR:%02X,DATA:%02X\r\n",addr,data);
     _pwire->beginTransmission(DDS_UNIT_I2CADDR);
     _pwire->write(addr);
@@ -10,7 +12,8 @@ void Unit_DDS::writeDDSReg(uint8_t addr, uint8_t data) {
 }
 
 /*! @brief Write a certain length of data to the DDS.*/
-void Unit_DDS::writeDDSReg(uint8_t addr, uint8_t* data, size_t size) {
+void Unit_DDS::writeDDSReg(uint8_t addr, uint8_t* data, size_t size)
+{
     _pwire->beginTransmission(DDS_UNIT_I2CADDR);
     _pwire->write(addr);
     for (size_t i = 0; i < size; i++) {
@@ -20,7 +23,8 @@ void Unit_DDS::writeDDSReg(uint8_t addr, uint8_t* data, size_t size) {
 }
 
 /*! @brief Read data from the DDS.*/
-uint8_t Unit_DDS::readDDSReg(uint8_t addr) {
+uint8_t Unit_DDS::readDDSReg(uint8_t addr)
+{
     uint8_t data;
     _pwire->beginTransmission(DDS_UNIT_I2CADDR);
     _pwire->write(addr);
@@ -33,7 +37,8 @@ uint8_t Unit_DDS::readDDSReg(uint8_t addr) {
 }
 
 /*! @brief Read a certain length of data from the DDS.*/
-void Unit_DDS::readDDSRegs(uint8_t addr, uint8_t* dataptr, uint8_t size) {
+void Unit_DDS::readDDSRegs(uint8_t addr, uint8_t* dataptr, uint8_t size)
+{
     _pwire->beginTransmission(DDS_UNIT_I2CADDR);
     _pwire->write(addr);
     _pwire->endTransmission();
@@ -46,7 +51,8 @@ void Unit_DDS::readDDSRegs(uint8_t addr, uint8_t* dataptr, uint8_t size) {
 
 /*! @brief Initialize the DDS.
     @return Return 0 if the init was successful, otherwise -1.. */
-int Unit_DDS::begin(TwoWire* p) {
+int Unit_DDS::begin(TwoWire* p)
+{
     _pwire = p;
 
     char snStr[7];
@@ -61,7 +67,8 @@ int Unit_DDS::begin(TwoWire* p) {
 }
 
 /*! @brief Set the signal frequency.*/
-void Unit_DDS::setFreq(uint8_t reg, uint64_t freq) {
+void Unit_DDS::setFreq(uint8_t reg, uint64_t freq)
+{
     freq = freq * 268435456 / DDS_FMCLK;
 
     uint8_t sendbuff[4] = {0, 0, 0, 0};
@@ -73,7 +80,8 @@ void Unit_DDS::setFreq(uint8_t reg, uint64_t freq) {
 }
 
 /*! @brief Set the signal phase.*/
-void Unit_DDS::setPhase(uint8_t reg, uint32_t phase) {
+void Unit_DDS::setPhase(uint8_t reg, uint32_t phase)
+{
     uint8_t sendbuff[2] = {0, 0};
     phase               = phase * 2048 / 360;
     sendbuff[0] |= (((phase >> 8) & 0xff) | ((reg == 1) ? 0xC0 : 0x80));
@@ -84,8 +92,8 @@ void Unit_DDS::setPhase(uint8_t reg, uint32_t phase) {
 }
 
 /*! @brief Set the signal frequency and phase.*/
-void Unit_DDS::setFreqAndPhase(uint8_t freg, uint64_t freq, uint8_t preg,
-                               uint32_t phase) {
+void Unit_DDS::setFreqAndPhase(uint8_t freg, uint64_t freq, uint8_t preg, uint32_t phase)
+{
     uint8_t sendbuff[6] = {0, 0, 0, 0, 0, 0};
 
     freq = freq * 268435456 / DDS_FMCLK;
@@ -98,52 +106,64 @@ void Unit_DDS::setFreqAndPhase(uint8_t freg, uint64_t freq, uint8_t preg,
     sendbuff[4] |= (((phase >> 8) & 0xff) | ((preg == 1) ? 0xC0 : 0x80));
     sendbuff[5] |= (phase & 0xff);
 
+    std::printf("%02X:%02X:%02X:%02X:%02X:%02X\n", sendbuff[0], sendbuff[1], sendbuff[2], sendbuff[3], sendbuff[4],
+                sendbuff[5]);
+
     writeDDSReg(DDS_FREQ_ADDR, sendbuff, 6);
 }
 
 /*! @brief Set the type of output signal.*/
-void Unit_DDS::setMode(DDSmode mode) {
+void Unit_DDS::setMode(DDSmode mode)
+{
     writeDDSReg(DDS_MODE_ADDE, 0x80 | mode);
 }
 
 /*! @brief Set the signal frequency and phase.*/
-void Unit_DDS::setCTRL(uint8_t ctrlbyte) {
+void Unit_DDS::setCTRL(uint8_t ctrlbyte)
+{
     writeDDSReg(DDS_CTRL_ADDR, 0x80 | ctrlbyte);
 }
 
 /*! @brief Select frequency register.*/
-void Unit_DDS::selectFreqReg(uint8_t num) {
+void Unit_DDS::selectFreqReg(uint8_t num)
+{
     uint8_t reg = readDDSReg(DDS_CTRL_ADDR);
     reg &= (~0x40);
     writeDDSReg(DDS_CTRL_ADDR, reg | 0x80 | (num == 1) ? 0x40 : 0);
 }
 
 /*! @brief Select phase register.*/
-void Unit_DDS::selectPhaseReg(uint8_t num) {
+void Unit_DDS::selectPhaseReg(uint8_t num)
+{
     uint8_t reg = readDDSReg(DDS_CTRL_ADDR);
     reg &= (~0x20);
     writeDDSReg(DDS_CTRL_ADDR, reg | 0x80 | (num == 1) ? 0x20 : 0);
 }
 
 /*! @brief Output waveform with specified frequency and phase.*/
-void Unit_DDS::quickOUT(DDSmode mode, uint64_t freq, uint32_t phase) {
+void Unit_DDS::quickOUT(DDSmode mode, uint64_t freq, uint32_t phase)
+{
     if (mode <= kSQUAREMode) {
+        std::printf("====> setFreq\n");
         setFreqAndPhase(0, freq, 0, phase);
     }
     writeDDSReg(0x20, 0x80 | mode);
     writeDDSReg(0x21, 0x80);
+
+    std::printf("M:%02X C:%02X\n", 0x80 | mode, 0x80);
 }
 
 /*! @brief Output specified frequency and phase.*/
-void Unit_DDS::OUT(uint8_t freqnum, uint8_t phasenum) {
+void Unit_DDS::OUT(uint8_t freqnum, uint8_t phasenum)
+{
     uint8_t reg = readDDSReg(DDS_CTRL_ADDR);
     reg &= (~0x60);
-    writeDDSReg(DDS_CTRL_ADDR, reg | 0x80 | ((freqnum == 1) ? 0x40 : 0) |
-                                   ((phasenum == 1) ? 0x20 : 0));
+    writeDDSReg(DDS_CTRL_ADDR, reg | 0x80 | ((freqnum == 1) ? 0x40 : 0) | ((phasenum == 1) ? 0x20 : 0));
 }
 
 /*! @brief Set the signal frequency and phase.*/
-void Unit_DDS::setSleep(uint8_t level) {
+void Unit_DDS::setSleep(uint8_t level)
+{
     uint8_t reg = readDDSReg(DDS_CTRL_ADDR);
     reg &= (~0x18);
     reg |= (level == 1) ? 0x10 : 0;
@@ -152,6 +172,7 @@ void Unit_DDS::setSleep(uint8_t level) {
 }
 
 /*! @brief Reset DDS Unit.*/
-void Unit_DDS::reset() {
+void Unit_DDS::reset()
+{
     writeDDSReg(DDS_CTRL_ADDR, 0x80 | 0x04);
 }
